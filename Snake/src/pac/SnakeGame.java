35a51aee02ef55b8eaac7f802630ca06dc6fa60a
4ernoknijnik <pooh.adm@gmail.com>
2016-07-17 03:31:54 +0300

    Complete

--- Snake/src/pac/SnakeGame.java
+++ Snake/src/pac/SnakeGame.java
@@ -6,10 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
 import pacObj.Apple;
 import pacObj.GUI;
 import pacObj.Snake;
